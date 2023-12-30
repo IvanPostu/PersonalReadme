1a10d2ab3d1cb00b36b6d54549a80025fd9abc60
ivan <ipostu20000127@gmail.com>
2023-12-30 17:34:47 +0200

    Do lab 2

--- MongoDB/mongo-client.nix
+++ MongoDB/mongo-client.nix
@@ -1,14 +1,14 @@
-# nix-shell ./mongo-client.nix --run bash
-{ nixpkgs ? import <nixpkgs> {} }:
+{ nixpkgs ? import <nixpkgs> {
+   config.allowUnfree = true;
+} }:
 
 with nixpkgs;
 
-let jdk = pkgs.jdk17;
 
-in
 pkgs.stdenv.mkDerivation rec {
   name = "nix-mongo-shell";
   buildInputs = [
     mongosh
+    mongodb-compass
   ];
 }
