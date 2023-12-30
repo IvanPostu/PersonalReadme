3d8c3838efededcada23a97b833304f4902c17f4
ivan <ipostu20000127@gmail.com>
2023-12-30 23:10:54 +0200

    make progress

--- MongoDB/java-mongo-demo/src/main/java/ipostu/mongo/demo/part1/Part1.java
+++ MongoDB/java-mongo-demo/src/main/java/ipostu/mongo/demo/part1/Part1.java
@@ -10,9 +10,9 @@ import java.util.List;
 public class Part1 {
     public static void main(String[] args) {
         String connectionString = System.getProperty("mongodb.uri");
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            List<Document> databases = mongoClient.listDatabases().into(new LinkedList<>());
-            databases.forEach(db -> System.out.println(db.toJson()));
-        }
+        MongoClient mongoClient = MongoClients.create(connectionString);
+        List<Document> databases = mongoClient.listDatabases().into(new LinkedList<>());
+        databases.forEach(db -> System.out.println(db.toJson()));
+        mongoClient.close();
     }
 }
